7f7b13c46c85baa2dff947922d7f61cb739584ee
Diego Lima <37930956+diegocslima@users.noreply.github.com>
2019-09-10 17:01:37 -0300

    Update ArduLampBT.ino

--- ArduLampBT/ArduLampBT.ino
+++ ArduLampBT/ArduLampBT.ino
@@ -2,8 +2,8 @@ char buf;
  
 void setup()
 {
-  //Define o pino 13 como saida
-  pinMode(13, OUTPUT);
+  //Define o pino 12 como saida
+  pinMode(12, OUTPUT);
   Serial.begin(9600);
 }
  
@@ -15,13 +15,13 @@ void loop()
     //Caso seja recebido o caracter L, acende o led
     if (buf == 'L'|| buf == 'l')
    {
-     digitalWrite(13, HIGH);
+     digitalWrite(12, LOW);
      Serial.println("lampada ligada");
    }
     //Caso seja recebido o caracter D, apaga o led
      if (buf == 'D' || buf == 'd' )
     {
-      digitalWrite(13, LOW);
+      digitalWrite(12, HIGH);
       Serial.println("lampada desligada");
     }
   }
