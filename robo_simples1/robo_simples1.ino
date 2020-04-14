d8ed3febc895597ca5e9bfd6ac1cc080d3d229a4
Diego Lima <37930956+diegocslima@users.noreply.github.com>
2020-04-14 10:40:36 -0300

    Otimização

--- robo_simples1/robo_simples1.ino
+++ robo_simples1/robo_simples1.ino
@@ -1,9 +1,5 @@
 int dist = 0;
 
-int i = 0;
-
-int j = 0;
-
 long readUltrasonicDistance(int triggerPin, int echoPin)
 {
   pinMode(triggerPin, OUTPUT);  // Clear the trigger
@@ -20,24 +16,28 @@ long readUltrasonicDistance(int triggerPin, int echoPin)
 
 void setup()
 {
+  pinMode(A0, INPUT);
   Serial.begin(9600);
 
-  pinMode(4, OUTPUT);
+  pinMode(6, OUTPUT);
   pinMode(5, OUTPUT);
 }
 
 void loop()
 {
-  dist = 0.01723 * readUltrasonicDistance(11, 12);
+  dist = 0.01723 * readUltrasonicDistance(10, 12);
   Serial.println(dist);
-  if (dist > 20) {
-    digitalWrite(4, HIGH);
+
+  if (dist > 30) {
+    digitalWrite(6, HIGH);
     digitalWrite(5, HIGH);
   } else {
     digitalWrite(5, LOW);
-    digitalWrite(4, LOW);
-    delay(1000); // Wait for 1000 millisecond(s)
-    digitalWrite(4, HIGH);
-    delay(500); // Wait for 1000 millisecond(s)
+    digitalWrite(6, LOW);
+    delay(800); // Wait for 500 millisecond(s)
+    digitalWrite(6, HIGH);
+    delay(500); // Wait for 500 millisecond(s)
+    digitalWrite(6, LOW);
+    delay(500);
   }
 }
